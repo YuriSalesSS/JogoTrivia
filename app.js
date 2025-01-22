a3911bf41c1f6fa881e12f01fd7964c7f8cb7ced
Yuri <yurisales2000@hotmail.com>
2025-01-22 19:53:21 -0300

    inicio da seleção de tema, criação de funcao para converter o ID do botao e utilizar o valor em conjunto com o JSON

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 import { carregarPerguntas } from './jsonLoader.js'
 const botoes = document.querySelectorAll('.botao')
 
-
-carregarPerguntas().then(data =>{
-  console.log('Dados carregados:', data)
-
+function conversaoId(callback){
   botoes.forEach(function(botao){
     botao.addEventListener('click', function(){
-      if (botao.id === 'HistoriaBt'){
-        console.log('Perguntas sobre Historia')
-      }else if (botao.id === 'GeografiaBt'){
-        console.log('Perguntas sobre Geografia')
-      }else if (botao.id === 'CienciasBt'){
-        console.log('Perguntas sobre Ciencias')
-      }else if(botao.id === 'ConhecimentosGBt'){
-        console.log('Perguntas sobre Conhecimentos Gerais')
-      }
+      let valor = botao.id
+      valor = valor.replace("Bt","").toLowerCase()
+      callback(valor) 
     })
+     
   })
+ 
+}
+
+
+carregarPerguntas().then(data =>{
+    conversaoId(function(valorConvertido){
+      console.log(valorConvertido)
+    })
 }).catch(error => {
   console.error('Erro ao carregar as perguntas:', error.message);
-});
+})
+
+
 
   
