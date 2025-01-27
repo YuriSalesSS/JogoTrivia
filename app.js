f44afbca4c2538063581f05e2fa30a7aa9b739c7
Yuri <yurisales2000@hotmail.com>
2025-01-27 19:26:05 -0300

    refatoração do codigo

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 import { carregarPerguntas } from './jsonLoader.js'
-const botoes = document.querySelectorAll('.botao')
-const perguntas = document.getElementById('perguntas')
 
-carregarPerguntas().then(data =>{
-    conversaoId(function(valorConvertido){
-      let pergunta = data[valorConvertido][0].pergunta
-      perguntas.innerText = `${pergunta}`
-        
-    })
-}).catch(error => {
+const botoes = document.querySelectorAll('.botao')
+const perguntasContainter = document.getElementById('perguntas')
+
+// funcao principal
+carregarPerguntas()
+  .then(data =>{
+    console.log('Perguntas carregadas', data)
+    configurarBotoes(data)
+})
+.catch(error => {
   console.error('Erro ao carregar as perguntas:', error.message)
 })
 
 
-function conversaoId(callback){
-  botoes.forEach(function(botao){
-    botao.addEventListener('click', function(){
-      let valor = botao.id
-      valor = valor.replace("Bt","").toLowerCase()
-      callback(valor) 
+function configurarBotoes(data){
+    botoes.forEach(botao => {
+      botao.addEventListener('click', () => {
+        const categoria = botao.id.replace("Bt", "").toLowerCase()
+        exibirPergunta(data, categoria)
+      })
     })
      
-  })
+  
  
 }
 
+function exibirPergunta(data, categoria){
+  if (data[categoria] && data[categoria][0]){
+    const pergunta = data[categoria][0].pergunta
+    perguntasContainter.innerText = pergunta
+  } else {
+    perguntasContainter.innerText = 'Categoria não encontrada'
+  }
+}
